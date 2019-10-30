2695842e245d936485a393ec3afcff0bc6dd4440
Ryan Coleman <AlmightyOcho@users.noreply.github.com>
2019-10-30 00:23:33 -0500

    Add files via upload

---
+++ FavoriteQuotes.java
@@ -0,0 +1,6 @@
+public class FavoriteQuotes {
+    public static void main(String[] args) {
+        System.out.println("\"If it don't make dollas, then it don't make cents.\"\n  - Benjamin Franklin, The White House\n");
+        System.out.println("\"There ain’t gonna be nothing the same. I’m gonna try to go another way. I like to keep going different ways and keep it the same. I don’t copy. I come up with my own stuff, my own everything.\"\n  - Chief Keef, The Story of O'Block");
+    }
+}
